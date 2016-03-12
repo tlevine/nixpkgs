8f999ab909b7d069d0146a9792e7414a2b96806e
Thomas Levine <_@thomaslevine.com>
2016-03-12 11:46:19 +0000

    gobi_loader: add Windows driver url

--- pkgs/os-specific/linux/gobi_loader/default.nix
+++ pkgs/os-specific/linux/gobi_loader/default.nix
@@ -6,6 +6,10 @@ stdenv.mkDerivation {
     url = "http://www.codon.org.uk/~mjg59/gobi_loader/download/gobi_loader-0.7.tar.gz";
     sha256 = "78bdc255451cde1caa406e146b01a88828480c9c43272de8cffdb61627be754a";
   };
+  driver = fetchurl {
+    url = "https://download.lenovo.com/ibmdl/pub/pc/pccbbs/mobiles/7xwc48ww.exe";
+    sha256 = "0ebe933da62bb38db556a4a20d25f4109cc1ae062ddce2f679059628e2487aeb";
+  }
   meta = {
     description = "IBM ThinkPad hardware functions driver";
     homepage = "http://www.codon.org.uk/~mjg59/gobi_loader/";
