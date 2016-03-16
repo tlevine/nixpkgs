8abedbe20a3c66142a6d56874433c941ceef4038
Thomas Levine <_@thomaslevine.com>
2016-03-16 23:29:36 +0000

    pythonPackages.plyvel: Start the package.

---
+++ pkgs/development/python-modules/plyvel/default.nix
@@ -0,0 +1,18 @@
+{ stdenv, fetchurl, python, leveldb }:
+
+buildPythonPackage rec {
+  version = "0.9";
+  name = "plyvel-${version}";
+
+  src = fetchurl {
+    url = "https://pypi.python.org/packages/source/p/plyvel/plyvel-0.9.tar.gz";
+    sha256 = "587d93681ae44936ae086b4b45486eb302e3853ba5af149aac3be9e9713998e9";
+  };
+
+  buildInputs = [ python leveldb ];
+  meta = {
+    description = "a fast and feature-rich Python interface to LevelDB";
+    homepage = "https://plyvel.readthedocs.org/";
+    license = stdenv.lib.licenses.bsd3;
+  };
+}
