0cbaab49b0e20312993625333841644d5b8cb4d0
Thomas Levine <_@thomaslevine.com>
2016-03-16 23:48:08 +0000

    pythonPackages.plyvel: Don't run tests.

--- pkgs/development/python-modules/plyvel/default.nix
+++ pkgs/development/python-modules/plyvel/default.nix
@@ -1,15 +1,16 @@
-{ stdenv, fetchurl, python, buildPythonPackage, leveldb, cython }:
+{ stdenv, fetchurl, buildPythonPackage, leveldb, cython }:
 
 buildPythonPackage rec {
   version = "0.9";
   name = "plyvel-${version}";
 
   src = fetchurl {
-    url = "https://pypi.python.org/packages/source/p/plyvel/plyvel-0.9.tar.gz";
+    url = "https://pypi.python.org/packages/source/p/plyvel/${name}.tar.gz";
     sha256 = "587d93681ae44936ae086b4b45486eb302e3853ba5af149aac3be9e9713998e9";
   };
 
-  buildInputs = [ python leveldb cython ];
+  doCheck = false;
+  buildInputs = [ leveldb cython ];
   meta = {
     description = "a fast and feature-rich Python interface to LevelDB";
     homepage = "https://plyvel.readthedocs.org/";
